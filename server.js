e4a5b5f406c0121e15120a7248f975c2373a7920
vijithahmbandara <vijithahmbandara@gmail.com>
2024-12-11 22:54:54 +0530

    signup user

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
+const users = require('./data.js');
 const app = express();
+const fs = require('fs')
+app.use(express.json())
+const path = require('path');
+const filePath = path.join(__dirname, 'data.js');
 dotenv.config();
+app.use(cors());
 
 const port = process.env.PROT || 5000
 
@@ -29,9 +36,47 @@ const pizzas = [
     }
   ];
 
+const writeDataToFile = () => {
+    const data = `const users = ${JSON.stringify(users, null, 2)};\n\nmodule.exports = users;`;
+    fs.writeFileSync(filePath, data, 'utf8');
+};
+
 app.get('/products',(req, res) => {
     res.status(200).send(pizzas)
 })
+
+/*
+const createUser = (newUser) => {
+  users.push(newUser);
+  writeDataToFile();
+  console.log('✅ User added successfully:', newUser);
+};
+*/
+
+app.post('/user', (req, res) => {
+  
+  console.log(req.body)
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required (id, name, email, password)' });
+  }
+
+  const existingUser = users.find(user => user.email === email);
+  if (existingUser) {
+      return res.status(400).json({ message: 'User with this email already exists' });
+  }
+
+  const newUser = {name, email, password, position:"empolyee" };
+  users.push(newUser);
+  writeDataToFile();
+
+  res.status(201).json({
+      message: 'User created successfully',
+      user: newUser
+  });
+});
+
 
 app.listen(port, () => {
     console.log(`app run on port ${port}`)
